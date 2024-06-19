b013ada00fc5705d623903b9ca658c992c63cc87
박준식 <qwt2379@naver.com>
2024-06-19 14:20:34 +0900

    test2

--- src/main/java/com/smhrd/foodie/controller/MemberController.java
+++ src/main/java/com/smhrd/foodie/controller/MemberController.java
@@ -1,14 +1,21 @@
 package com.smhrd.foodie.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smhrd.foodie.mapper.MemberMapper;
+import com.smhrd.foodie.model.Member;
 	
 @Controller
 public class MemberController {
 	
-	// 회원가입
+	@Autowired
+	MemberMapper mapper;
+	
+	// 회원가입 페이지
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() {
 		return "join";
@@ -20,20 +27,49 @@ public class MemberController {
 			@RequestParam("pwck") String pwck, @RequestParam("email") String email,
 			@RequestParam("tel") String tel, @RequestParam("addr") String addr) {
 		
+		
 		System.out.println("응답받은 ID : " + id);
 		System.out.println("응답받은 PW : " + pw);
 		System.out.println("응답받은 PWCK : " + pwck);
 		System.out.println("응답받은 email : " + email);
 		System.out.println("응답받은 tel : " + tel);
 		System.out.println("응답받은 addr : " + addr);
-
+		
+		
+		
+		Member member = new Member(id, pw, pwck, email, tel, addr);
+		
+		System.out.println("member 값 : " + member);
+		
+		// sql문 실행
+		// 1. mapper에 있는 userJoin 메소드 실행
+		// 흐름 : contoller -> 인터페이스 -> xml -> controller로 값 반환
+		int row = mapper.userJoin(member);
+		
+		System.out.println(row);
+		
+		if(row > 0) {
+			System.out.println("회원가입 성공");
+		}else {
+			System.out.println("회원가입 실패");
+		}
+		
 		return "login";
 	}
 
-	// 로그인
+	// 로그인 페이지
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
+	}
+	
+	// 로그인 확인 메소드
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String loginCheck() {
+		
+		
+		
+		return "index";
 	}
 	
 	// 마이페이지
@@ -42,27 +78,31 @@ public class MemberController {
 		return "mypage";
 	}
 	
-	// 찜목록
+	// 찜목록 페이지
 	@RequestMapping(value = "/wishlist", method = RequestMethod.GET)
 	public String wishlist() {
 		return "wishlist";
 	}
 
-	// 마이페이지에서 회원 정보 변경
+	// 마이페이지에서 회원 정보 변경 페이지
 	@RequestMapping(value = "/edituserinfo", method = RequestMethod.GET)
 	public String edituserinfo() {
 		return "edituserinfo";
 	}
 
+	// 알러지 페이지
 	@RequestMapping(value = "/allergy", method = RequestMethod.GET)
 	public String allergy() {
 		return "allergy";
 	}
+	
+	// 회원탈퇴 페이지
 	@RequestMapping(value = "/deleteuserinfo", method = RequestMethod.GET)
 	public String deleteuserinfo() {
 		return "deleteuserinfo";
 	}
-
+	
+	// 비밀번호 변경 페이지
 	@RequestMapping(value = "/editpw", method = RequestMethod.GET)
 	public String editpw() {
 		return "editpw";
--- src/main/java/com/smhrd/foodie/mapper/MemberMapper.java
+++ src/main/java/com/smhrd/foodie/mapper/MemberMapper.java
@@ -2,17 +2,23 @@ package com.smhrd.foodie.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.smhrd.foodie.model.Member;
+
+
+
+
 @Mapper
 public interface MemberMapper {
 	
+	// 얘네는 사실 프록시 객체임
 	
-	void select();
+	// 2. 얘가 실행이 되면 Mybatis의 프록시 객체가
+	// 해당 인터페이스의 namespace와 userJoin 메서드 이름에 해당하는 xml파일 찾음
+	int userJoin(Member member);
 	
-	void join();
+	int login();
 	
-	void login();
-	
-	void editPw();
+	int editPw();
 	
 	void delete();
 	
